bf00ab39b09cfe76921392932f232e452a502238
Jaidev Singh <jaidev.bhui@gmail.com>
2019-06-30 14:02:00 +0530

    added mode buttons functionality

--- cg.js
+++ cg.js
@@ -1,11 +1,12 @@
 var colors = [];
+var correctColor;
+var currentMode = 6;
 var colorDisplay = document.getElementById("colorDisplay");
 var colorBox = document.querySelectorAll(".colorBox");
 var resultDisplay = document.getElementById("resultDisplay");
 var resetButton = document.getElementById("resetButton");
 var h1 = document.querySelector("h1");
-var correctColor;
-
+var modes = document.querySelectorAll(".modeButton");
 
 // var num = 6;     
 
@@ -15,14 +16,21 @@ init();
 function init(){
     colorGrid();
     circles();
+    mode();
 }
 
 function colorGrid(){
-    colors = createColors(6);
-    correctColor = colors[Math.floor(Math.random() * 6)];
+    colors = createColors(currentMode);
+    correctColor = colors[Math.floor(Math.random() * currentMode)];
     colorDisplay.textContent = correctColor;
     for (let i = 0; i < colorBox.length; i++) {
-        colorBox[i].style.backgroundColor = colors[i];
+        if(colors[i]){
+            colorBox[i].style.display = "block";
+            colorBox[i].style.backgroundColor = colors[i];
+        }
+        else{
+            colorBox[i].style.display = "none";
+        }
     }
 }
 
@@ -54,12 +62,15 @@ function winColors(color){
     }
 }
 
-resetButton.addEventListener("click",function reset(){
+resetButton.addEventListener("click",reset);
+
+function reset(){
     colorGrid();
     circles();
     resultDisplay.textContent = "";
     h1.style.backgroundColor = "steelblue";    
-});
+}
+
 
 function createColors(num){
     var arr = [];
@@ -67,7 +78,19 @@ function createColors(num){
     arr.push(randomColor());
     }
     return arr;
+}
 
+function mode(){
+    for (let i = 0; i < modes.length; i++) {
+        modes[i].addEventListener("click", function(){
+            modes[0].classList.remove("selected");
+            modes[1].classList.remove("selected");
+            this.classList.add("selected");
+            this.textContent === "Easy" ? currentMode = 3: currentMode = 6;
+        reset();
+        });
+        
+    }
 }
 
 function randomColor(){
